ec3d7e6b31a9a2f5b11c78b13c355c08d4826ac7
Holger Graef <holger.graef@parrot.com>
2023-08-05 18:25:45 +0200

    fix bind mounts

--- user/chroot-helper.sh
+++ user/chroot-helper.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -ex
 
+RPI_ROOTFS_BASE=/opt/qemu-rpi/sysroot
+
 mount --bind /sys ${RPI_ROOTFS_BASE}/sys
 mount --bind /proc ${RPI_ROOTFS_BASE}/proc
 mount --bind /dev ${RPI_ROOTFS_BASE}/dev
 mount --bind /dev/pts ${RPI_ROOTFS_BASE}/dev/pts
+mount --bind /tmp ${RPI_ROOTFS_BASE}/tmp
 
 /usr/sbin/chroot /opt/qemu-rpi/sysroot /usr/bin/qemu-aarch64-static /bin/bash
