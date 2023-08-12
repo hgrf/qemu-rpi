d3c497bff4a382fee22479f0ec305d11a0e16dc4
Holger Graef <holger.graef@parrot.com>
2023-08-12 11:12:24 +0200

    remove bind mount on /tmp

--- user/chroot-helper.sh
+++ user/chroot-helper.sh
@@ -8,6 +8,5 @@ mount --bind /sys ${RPI_ROOTFS_BASE}/sys
 mount --bind /proc ${RPI_ROOTFS_BASE}/proc
 mount --bind /dev ${RPI_ROOTFS_BASE}/dev
 mount --bind /dev/pts ${RPI_ROOTFS_BASE}/dev/pts
-mount --bind /tmp ${RPI_ROOTFS_BASE}/tmp
 
 /usr/sbin/chroot /opt/qemu-rpi/sysroot /usr/bin/qemu-aarch64-static /bin/bash $@
